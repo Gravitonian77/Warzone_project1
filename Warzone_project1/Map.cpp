06f968758fc9c2f63ea97f58972f337611bb293b
Gravitonian77 <122951929+Gravitonian77@users.noreply.github.com>
2023-12-21 03:12:28 -0600

    Add files via upload

---
+++ Warzone_project1/Map.cpp
@@ -0,0 +1,23 @@
+//
+// Created by garvi on 2023-12-20.
+//
+
+#include "Map.h"
+#include <iostream>
+#include "fstream"
+#include <map>
+
+using namespace std;
+
+MapLoader::MapLoader() = default;
+
+MapLoader::MapLoader(std::string map_file_name){
+	ifstream  file(map_file_name);
+	string line;
+	map<string, int> continents;
+	vector<string> terriotries;
+
+	if(file.is_open()){
+
+	}
+}
---
+++ Warzone_project1/Map.h
@@ -0,0 +1,37 @@
+//
+// Created by garvi on 2023-12-19.
+//
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#ifndef WARZONE_PROJECT1_MAP_H
+#define WARZONE_PROJECT1_MAP_H
+
+class Map{
+
+};
+
+class Territory{
+private:
+	Territory* next_territory;
+public:
+	Territory();
+	Territory(const Territory& t);
+};
+
+class MapLoader{
+public:
+	MapLoader();
+	MapLoader(string map_file_name);
+	MapLoader(const MapLoader& map);
+	const string &getMapFileName() const;
+	void setMapFileName(const string &mapFileName);
+
+private:
+	string map_file_name;
+};
+
+#endif //WARZONE_PROJECT1_MAP_H
